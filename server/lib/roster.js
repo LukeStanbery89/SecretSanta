01ff2b9769902995d2da2b774e3b7dbb152162c4
Luke Stanbery <lukestanbery89@gmail.com>
2022-11-07 18:00:45 -0600

    write logic to assign secret santas

--- server/lib/roster.js
+++ server/lib/roster.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const assert = require('node:assert');
+const { shuffle } = require('../utils/data-utils');
 const { sendSMS } = require('./sms');
 
 function handleRosterSubmission(params) {
@@ -13,36 +14,69 @@ function handleRosterSubmission(params) {
 function _validate(params) {
     const {res, roster} = params;
     return new Promise((resolve, reject) => {
-        const schema = {
-            name: 'string',
-            phoneNumber: new RegExp('[0-9]{3}-[0-9]{3}-[0-9]{4}'),
-        };
-        roster.map(p => {
-            for (const [key, value] of Object.entries(schema)) {
-                try {
-                    assert(p[key]);
-                    switch(value.constructor.name) {
-                    case 'RegExp':
-                        assert(value.test(p[key]));
-                        break;
-                    default:
-                        assert(typeof p[key] === value);
-                        break;
-                    }
-                } catch (e) {
-                    console.error({
-                        key,
-                        format: value,
-                        value: p[key],
-                        error: e,
-                    });
-                    res.statusMessage = 'Invalid input';
-                    res.status(400).end();
-                    reject('Invalid input');
-                }
-            }
-        });
+        _validateRosterSize(roster, res, reject);
+        _validateSchema(roster, res, reject);
+        _validateNoDuplicatePhoneNumbers(roster, res, reject);
         resolve(params);
+    });
+}
+
+function _validateRosterSize(roster, res, reject) {
+    if (roster.length <= 2) {
+        res.statusMessage = 'Invalid input schema';
+        res.status(400).end();
+        reject('Not enough participants');
+    }
+}
+
+function _validateSchema(roster, res, reject) {
+    const schema = {
+        name: 'string',
+        phoneNumber: new RegExp('[0-9]{3}-[0-9]{3}-[0-9]{4}'),
+    };
+    roster.map(p => {
+        for (const [key, value] of Object.entries(schema)) {
+            try {
+                assert(p[key]);
+                switch (value.constructor.name) {
+                case 'RegExp':
+                    assert(value.test(p[key]));
+                    break;
+                default:
+                    assert(typeof p[key] === value);
+                    break;
+                }
+            } catch (e) {
+                console.error({
+                    key,
+                    format: value,
+                    value: p[key],
+                    error: e,
+                });
+                res.statusMessage = 'Invalid input schema';
+                res.status(400).end();
+                reject('Invalid input schema');
+            }
+        }
+    });
+}
+
+function _validateNoDuplicatePhoneNumbers(roster, res, reject) {
+    if (process.env.ALLOW_DUPLICATE_PHONE_NUMBERS === true || process.env.ALLOW_DUPLICATE_PHONE_NUMBERS === 'true') {
+        return;
+    }
+    let phoneMap = {};
+    roster.map(participant => {
+        if (phoneMap[participant.phoneNumber]) {
+            phoneMap[participant.phoneNumber]++;
+            if (phoneMap[participant.phoneNumber] > 1) {
+                res.statusMessage = 'Duplicate phone numbers';
+                res.status(400).end();
+                reject('Duplicate phone numbers');
+            }
+        } else {
+            phoneMap[participant.phoneNumber] = 1;
+        }
     });
 }
 
@@ -65,8 +99,28 @@ function _formatPhoneNumbers(params) {
 function _assignSecretSantas(params) {
     const {roster} = params;
     return new Promise((resolve) => {
-        // TODO
-        let rosterWithSecretSantas = [...roster];
+        /**
+         * The secret santa assignment algorithm works like this...
+         *
+         * 1. Create a copy of the original roster (to avoid mutation).
+         * 2. Shuffle the copy. This is where we get our randomness from.
+         * 3. Iterate over the shuffled copy and assign shuffledRoster[n + 1]
+         *    to shuffledRoster[n]. This ensures that no recipient is
+         *    assigned to themself, no one is assigned twice, and no one
+         *    gives and receives with the same person.
+         *
+         * TODO: Integrate blacklist functionality so that gifters are not
+         * assigned recipients they are not supposed to receive. This is
+         * good, for example, for couples who intend to give each other gifts
+         * independently of secret santa.
+         */
+        let shuffledRoster = shuffle([...roster]);
+        let rosterWithSecretSantas = shuffledRoster.map((participant, i) => {
+            return {
+                ...participant,
+                secretSanta: shuffledRoster[i + 1] ? shuffledRoster[i + 1] : shuffledRoster[0],
+            };
+        });
         resolve({
             ...params,
             roster: rosterWithSecretSantas,
@@ -85,7 +139,7 @@ function _dispatchAssignmentMessages(params) {
 function _constructSMSMessage(participant) {
     return {
         to: participant.phoneNumber,
-        body: `Hi, ${participant.name}! Welcome to SECRET SANTA! Your secret santa recipient is...\n\n${participant.secretSanta}`,
+        body: `Hi, ${participant.name}! Welcome to SECRET SANTA! Your secret santa recipient is...\n\n${participant.secretSanta.name}`,
     };
 }
 
